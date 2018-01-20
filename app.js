30097e9a46a65c68d0bf04ec438e0fb927f3af5d
Dani Vijay <danimvijay@gmail.com>
2018-01-20 17:12:08 +0530

    data and methods

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 new Vue({
-  
+  el: '#vue-app',
+  data: {
+    fname: 'Dani',
+    profile: {
+      lname: 'Vijay'
+    }
+  },
+  methods: {
+    greetme: function(time) {
+      return 'Good ' + time + ' ' + this.fname
+    }
+  }
 })
