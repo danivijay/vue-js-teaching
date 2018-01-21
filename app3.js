7b31168de879014abd894418d53b20f8a7926f6d
danivijay <danimvijay@gmail.com>
2018-01-21 21:22:16 +0530

    keyup event

---
+++ app3.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        
+    },
+    methods: {
+        logName: function (event) {
+            console.log(event.target.value)
+        },
+        logAge: function (event) {
+            console.log(event.target.value)
+        }
+    }
+})
