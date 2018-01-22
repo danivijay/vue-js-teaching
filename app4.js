d493aecebef3f10a6414df7b35786b67fc02c1a3
Dani Vijay <danimvijay@gmail.com>
2018-01-22 19:04:30 +0530

    if else and show

--- app4.js
+++ app4.js
@@ -3,9 +3,19 @@ new Vue({
     data: {
         name: '',
         person: {
-            age: ''
+            age: 0
         }
     },
     methods: {
+    },
+    computed: {
+        calcAge: function() {
+            return parseInt(this.person.age) + 18;
+        },
+        addRed: function() {
+            return {
+                red: (this.calcAge > 25 ? true : false)
+            }
+        }
     }
 })
---
+++ app5.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#vue-app',
+    data: {    
+        err: false,
+        succ: false
+    }
+})
