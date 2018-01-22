7e7237292d186375a28fba9f110e4c9ff78f1722
Dani Vijay <danimvijay@gmail.com>
2018-01-22 19:34:00 +0530

    for loop

--- app5.js
+++ app5.js
@@ -2,6 +2,12 @@ new Vue({
     el: '#vue-app',
     data: {    
         err: false,
-        succ: false
+        succ: false,
+        vehicles: ['bus', 'car', 'bike'],
+        carbrands: [
+            {name: 'hyunadai', country: 'korea'},
+            {name: 'audi', country: 'germany'},
+            {name: 'bmw', country: 'germany'}
+        ]
     }
 })
